3d2183ae5467bbdbb3f8981346b67df3ea050cc3
Bret Little <bret.little@gmail.com>
2014-09-18 16:49:05 -0600

    Update ignore

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,6 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage', 'junit'],
 
-
     // web server port
     port: 9876,
 
